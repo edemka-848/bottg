937186ed32f9e5c54c63c81d6c2eb00aca91576f
edemka-848 <146759616+edemka-848@users.noreply.github.com>
2024-06-16 15:59:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,11 @@ def send_welcome(message):
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+    
 
 
 bot.infinity_polling()
-
